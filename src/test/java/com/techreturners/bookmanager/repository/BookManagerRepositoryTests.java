8b31e0589ecc99e70bd09e55a56ab9b71d4b63c7
Swapna V <vswapna3202@gmail.com>
2023-09-21 17:38:34 +0100

    feat: added test case for repo

--- src/test/java/com/techreturners/bookmanager/repository/BookManagerRepositoryTests.java
+++ src/test/java/com/techreturners/bookmanager/repository/BookManagerRepositoryTests.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
@@ -35,6 +37,23 @@ public class BookManagerRepositoryTests {
         var bookById = bookManagerRepository.findById(book.getId());
         assertThat(bookById).isNotNull();
 
+    }
+
+    @Test
+    public void testDeleteBookByIdReturnsBoolean(){
+        //Insert new book
+        Book book = new Book(1L, "Book One", "This is the description for Book 1","Person One", Genre.Fiction);
+        bookManagerRepository.save(book);
+        //Fetch book on id and check if it is not null and exists
+        Optional<Book> bookById = bookManagerRepository.findById(book.getId());
+        assertThat(bookById).isNotNull();
+
+        //delete the inserted book
+        bookManagerRepository.deleteById(book.getId());
+        //Fetch book on id and check if it is empty and does not exist
+        Optional<Book> bookByIdAfterDeletion =
+                bookManagerRepository.findById(book.getId());
+        assertThat(bookByIdAfterDeletion).isEmpty();
     }
 
 }
