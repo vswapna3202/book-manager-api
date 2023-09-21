8d010c7c272e2dd557a8e7cb09ba855815b45a30
Swapna V <vswapna3202@gmail.com>
2023-09-21 18:51:30 +0100

    feat: Fixed issue in repo code

--- src/test/java/com/techreturners/bookmanager/repository/BookManagerRepositoryTests.java
+++ src/test/java/com/techreturners/bookmanager/repository/BookManagerRepositoryTests.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
 
 @DataJpaTest
 public class BookManagerRepositoryTests {
@@ -40,20 +41,19 @@ public class BookManagerRepositoryTests {
     }
 
     @Test
-    public void testDeleteBookByIdReturnsBoolean(){
-        //Insert new book
-        Book book = new Book(1L, "Book One", "This is the description for Book 1","Person One", Genre.Fiction);
+    public void testDeleteBookById(){
+
+        Book book = new Book(2L, "Book Two", "This is the description for Book Two", "Person Two", Genre.Fantasy);
         bookManagerRepository.save(book);
-        //Fetch book on id and check if it is not null and exists
-        Optional<Book> bookById = bookManagerRepository.findById(book.getId());
+
+        var bookById = bookManagerRepository.findById(book.getId());
         assertThat(bookById).isNotNull();
 
         //delete the inserted book
         bookManagerRepository.deleteById(book.getId());
-        //Fetch book on id and check if it is empty and does not exist
-        Optional<Book> bookByIdAfterDeletion =
-                bookManagerRepository.findById(book.getId());
-        assertThat(bookByIdAfterDeletion).isEmpty();
+
+        var bookAfterDeletion = bookManagerRepository.findById(book.getId());
+        assertThat(bookAfterDeletion).isEmpty();
     }
 
 }
