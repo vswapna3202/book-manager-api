6644fd4107f4815e594b687b3b2b2b2efa844410
Swapna V <vswapna3202@gmail.com>
2023-09-21 19:01:28 +0100

    feat: refactored code

--- src/test/java/com/techreturners/bookmanager/repository/BookManagerRepositoryTests.java
+++ src/test/java/com/techreturners/bookmanager/repository/BookManagerRepositoryTests.java
@@ -7,10 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import java.util.Optional;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.verify;
 
 @DataJpaTest
 public class BookManagerRepositoryTests {
